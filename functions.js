3b4c69002992fefbed810e307dbe79df08806f19
kevin <kevin.olson1836@gmail.com>
2022-06-24 21:16:24 -0500

    made good progress on website

---
+++ functions.js
@@ -0,0 +1,22 @@
+function moveScroller() {
+    var $anchor = $("#scroller-anchor");
+    var $scroller = $('#scroller');
+
+    var move = function() {
+        var st = $(window).scrollTop();
+        var ot = $anchor.offset().top;
+        if(st > ot) {
+            $scroller.css({
+                position: "fixed",
+                top: "0px"
+            });
+        } else {
+            $scroller.css({
+                position: "relative",
+                top: ""
+            });
+        }
+    };
+    $(window).scroll(move);
+    move();
+}
