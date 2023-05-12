7f8d83b0bbf4b0fa6b63c74c1012ab4cd57dd8d4
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2023-05-12 17:36:41 -0500

    made some style changes

--- assets/dist/js/jquery.js
+++ assets/dist/js/jquery.js
@@ -7,19 +7,19 @@ var height = 0;
 var width = 0;
 var increaseSize = 1.2;
 
-function checkIfVis(element){
-    // if box is visable and have only triggered once 
-    if ($('#boxxxxx').visible( true ) && shouldFadeIn == 0){
-        console.log("is vis");
-        shouldFadeIn = 1;
-        clearTimeout(checkIfVis );
+// function checkIfVis(element){
+//     // if box is visable and have only triggered once 
+//     if ($('#boxxxxx').visible( true ) && shouldFadeIn == 0){
+//         console.log("is vis");
+//         shouldFadeIn = 1;
+//         clearTimeout(checkIfVis );
 
-        // slide the element in (in mili seconds)
-        $("#boxxxxx").animate({marginLeft: "20%"}, 3000);
+//         // slide the element in (in mili seconds)
+//         $("#boxxxxx").animate({marginLeft: "20%"}, 3000);
 
-    }
-    setTimeout(checkIfVis, 500);
-}
+//     }
+//     setTimeout(checkIfVis, 500);
+// }
 
 function setProjectVars(){
     height = $(document.getElementById("firstProjectLink")).height();
@@ -28,7 +28,7 @@ function setProjectVars(){
 
 
 $(document).ready(function(){
-    checkIfVis()
+    // checkIfVis()
     setProjectVars();
 });
 
@@ -38,9 +38,12 @@ function growElement(element){
                         'transform': '      scale(' + increaseSize + ',' + increaseSize + ')',
                        
                         'padding': '0',
-                        'z-index': '1000'
+                        'z-index': '1000',
+                        'box-shadow': 'px 0px 6px 3px inset;'
                    });
     $(element).find("div").first().css({ 'background-color': '#e8f1e1'});
+    $(element).find("div").first().css('box-shadow', '10px 10px 5px #888');
+
     
 };
 
@@ -51,7 +54,8 @@ function shrinkElement(element){
                         'transform': 'scale(1,1)',
                         'border': 'none',
                         'padding': '5',
-                        'z-index': '0'
+                        'z-index': '0',
+                        'box-shadow': '0px 0px 2px 3px rgba(182, 190, 177,0.2) inset;'
                     });
     $(element).find("div").first().css({ 'background-color': 'white'});
 }
