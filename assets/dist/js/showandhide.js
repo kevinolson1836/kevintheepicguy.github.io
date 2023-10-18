7085b7417974d89b902cd0a925ef379c230071a1
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2023-10-17 20:40:20 -0500

    made some animation changes

--- assets/dist/js/showandhide.js
+++ assets/dist/js/showandhide.js
@@ -1,6 +1,8 @@
 
 
 function hideDiv(divID){
+    // document.getElementById(divID).onmouseout = function() {clearTimeout(timeout)};
+
     setTimeout(function(){
         var mainDiv = document.getElementById(divID);
         var h1Div = document.getElementById(divID).children[0];
@@ -20,39 +22,58 @@ function hideDiv(divID){
         $(leftSideContent).fadeTo(0, 0.7);
 
         hideDiv.style.display = "none";
+        h1Div.innerHTML = h1Div.innerHTML.replace("► ", "");
         h1Div.style.marginLeft = "0px";
         h1Div.style.fontWeight = "";
         mainDiv.style.backgroundColor = "#e2e8df";
         // leftSideContent.style.paddingLeft = "0%";
         // rightSideContent.style.paddingLeft = "0%";
-    }, 50);
+    }, 200);
 }
 
 
-function showDiv(divID){
+
+function _showDiv(divID){
     var mainDiv = document.getElementById(divID);
     var h1Div = document.getElementById(divID).children[0];
     var hideDiv = document.getElementById(divID).children[1];
     var leftSideContent = hideDiv.children[0].children[1];
     var rightSideContent = hideDiv.children[1].children[1];
     
-
+    
     $(leftSideContent).animate({ //be sure to change the class of your element to "header"
         paddingLeft:'15%',
     });
-
+    
     $(rightSideContent).animate({ //be sure to change the class of your element to "header"
         paddingLeft:'5%',
     });
-
+    
     $(rightSideContent).fadeTo(500, 1);
     $(leftSideContent).fadeTo(500, 1);
-
+    
     // leftSideContent.style.paddingLeft = "15%";
     rightSideContent.style.paddingLeft = "5%";
     hideDiv.style.display = "block";
+    h1Div.innerHTML = '► ' + h1Div.  innerHTML;
     h1Div.style.marginLeft = "100px";
     h1Div.style.fontWeight = "bold";
     mainDiv.style.backgroundColor = "#cfd9ca";
-    console.log(rightSideContent);
+    console.log("coc");
+}
+
+function showDiv(divID){
+    var timeout2 = ""
+    const timeout = setTimeout(function(){
+        _showDiv(divID);
+        clearTimeout(timeout2)
+    }, 205)
+
+    document.getElementById(divID).onmouseout = function() {
+        timeout2 = setTimeout(function(){
+            clearTimeout(timeout); 
+        },200)
+        
+        console.log("cleared")
+    };
 }
